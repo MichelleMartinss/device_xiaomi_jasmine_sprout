674172b160caff6c81b9daf5ca99dee8c5d1d76f
dianlujitao <dianlujitao@lineageos.org>
2020-02-21 12:38:05 +0800

    jasmine_sprout: Build AOSP USB HAL
    Change-Id: Id55b87c599054b732c4a33bf1f1fde192f3297b8

--- device.mk
+++ device.mk
@@ -402,6 +402,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
