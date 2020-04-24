49873f3bd7cbc649f6ddbfd52b46e539db69ef91
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    jasmine_sprout: Add JasmineWifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -459,6 +459,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
+    JasmineWifiOverlay \
     libwifi-hal-qcom \
     WifiOverlay \
     wpa_supplicant \
