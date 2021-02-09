95ec951d00234d4b484454ac719289c97d2165f4
Luca Stefani <luca020400@lineageos.org>
2021-02-09 11:32:18 +0100

    jasmine_sprout: set boot animation resolution
    - Device screen resolution is 2160*1080
    
    Change-Id: Ib1a3d2a4ed498199920f196f4f69e03de1128d0d

--- device.mk
+++ device.mk
@@ -131,6 +131,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2160
+TARGET_SCREEN_WIDTH := 1080
+
 # Biometrics
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sdm660
